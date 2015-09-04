c51f195a3f107cb3121c954ff4aa0b10892d97e5
conekilla69 <wrcfan@verizon.net>
2015-09-04 20:48:12 +0000

    I hope I got this right.

---
+++ new_python.py
@@ -0,0 +1,12 @@
+Python 2.7.6 (default, Jun 22 2015, 18:00:18) 
+[GCC 4.8.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> my_var ='some value'
+>>> my_new_var = my_var *2
+>>> 
+print my_var
+some value
+>>> print my_new_var
+some valuesome value
+>>> 
+
