29fcd67f9bdc2becebdd1b4a36389cfb12ea01b8
conekilla69 <wrcfan@verizon.net>
2015-09-08 03:04:48 +0000

    trying again.

--- new_python.py
+++ new_python.py
@@ -1,11 +1,13 @@
-Python 2.7.6 (default, Jun 22 2015, 18:00:18)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+Python 2.7.6  (default, Jun 22 2015,18:00:18)
 [GCC 4.8.2] on linux2
-Type "copyright", "credits" or "license()" for more information.
+Type "copyright", "credits" or "license" for more information.
 >>> my_var = 'some value'
 >>> my_new_var = my_var * 2
 >>> print my_new_var
 some valuesome value
 >>> print my_var
 some value
->>>
+
 
