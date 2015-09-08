d3ee1220c8d454a6ca768721445faeb400bd5821
conekilla69 <wrcfan@verizon.net>
2015-09-08 03:17:25 +0000

    trying again.

--- new_python.py
+++ new_python.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-Python 2.7.6  (default, Jun 22 2015,18:00:18)
+Python 2.7.6 (default, Jun 22 2015,18:00:18)
 [GCC 4.8.2] on linux2
 Type "copyright", "credits" or "license" for more information.
 >>> my_var = 'some value'
